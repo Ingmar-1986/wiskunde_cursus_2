2ec6c881acdb8bee50315e8a79270b73d3fbce29
Ingmar-1986 <herremaningmar@gmail.com>
2026-07-10 16:07:33 +0000

    Update cursus

--- injectcss.sh
+++ injectcss.sh
@@ -1,5 +1,5 @@
-
 for f in *.html
 do
-  sed -i '/standalone.css/a <link rel="stylesheet" href="global.css">' "$f"
+  sed -i '/standalone.css/a <link rel="stylesheet" href="base.css">' "$f"
+  sed -i '/base.css/a <link rel="stylesheet" href="global.css">' "$f"
 done
