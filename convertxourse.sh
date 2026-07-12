6266575cb379241f9897bd41fe108ca6be1ecdf8
Ingmar-1986 <herremaningmar@gmail.com>
2026-07-12 21:30:32 +0000

    Testmodule online test

--- convertxourse.sh
+++ convertxourse.sh
@@ -4,10 +4,18 @@ for f in testmodule/*.html
 do
     echo "Verwerk $f"
 
-    sed -i "s#<h1 class='card part' id='part1'>#<div class=\"activity-card card-sectionheading card part\"><div class=\"card-block\"><h4 class=\"card-title\">#g" "$f"
+    # PARTS
+    sed -i "
+s#<h1 class='card part' id='part1'>#<div class=\"activity-card card-sectionheading card part\"><div class=\"card-block\"><h4 class=\"card-title\">#g
+s#<h1 class='card part' id='part2'>#<div class=\"activity-card card-sectionheading card part\"><div class=\"card-block\"><h4 class=\"card-title\">#g
+s#<h1 class='card part' id='part3'>#<div class=\"activity-card card-sectionheading card part\"><div class=\"card-block\"><h4 class=\"card-title\">#g
+s#</h1>#</h4></div></div>#g
+" "$f"
 
-    sed -i "s#<h1 class='card part' id='part2'>#<div class=\"activity-card card-sectionheading card part\"><div class=\"card-block\"><h4 class=\"card-title\">#g" "$f"
-
-    sed -i "s#</h1>#</h4></div></div>#g" "$f"
+    # ACTIVITEITEN
+    sed -i "
+s#<a class='activity card '#<div class=\"activity-card card chapter\" #g
+s#</a>#</div>#g
+" "$f"
 
 done
