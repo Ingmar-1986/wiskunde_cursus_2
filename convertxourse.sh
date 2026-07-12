bd2f776ee49bf04b37db4a0c10ba6ed46d1c374c
Ingmar-1986 <herremaningmar@gmail.com>
2026-07-12 21:27:24 +0000

    Testmodule online test

---
+++ convertxourse.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for f in testmodule/*.html
+do
+    echo "Verwerk $f"
+
+    sed -i "s#<h1 class='card part' id='part1'>#<div class=\"activity-card card-sectionheading card part\"><div class=\"card-block\"><h4 class=\"card-title\">#g" "$f"
+
+    sed -i "s#<h1 class='card part' id='part2'>#<div class=\"activity-card card-sectionheading card part\"><div class=\"card-block\"><h4 class=\"card-title\">#g" "$f"
+
+    sed -i "s#</h1>#</h4></div></div>#g" "$f"
+
+done
