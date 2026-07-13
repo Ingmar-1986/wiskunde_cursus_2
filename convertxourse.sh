b144f6e07dd54a9dfb7f38d33e36b07ff704ded9
Ingmar-1986 <herremaningmar@gmail.com>
2026-07-13 07:12:50 +0000

    update

--- convertxourse.sh
+++ convertxourse.sh
@@ -4,24 +4,12 @@ for f in testmodule/*.html
 do
     echo "Verwerk $f"
 
-    # PARTS (Thema's)
+    # Parts omzetten naar KU Leuven-sectieblokken
     sed -i "
 s#<h1 class='card part' id='part1'>#<div class=\"activity-card card-sectionheading card part\"><div class=\"card-block\"><h4 class=\"card-title\">#g
 s#<h1 class='card part' id='part2'>#<div class=\"activity-card card-sectionheading card part\"><div class=\"card-block\"><h4 class=\"card-title\">#g
 s#<h1 class='card part' id='part3'>#<div class=\"activity-card card-sectionheading card part\"><div class=\"card-block\"><h4 class=\"card-title\">#g
 s#</h1>#</h4></div></div>#g
-" "$f"
-
-    # ACTIVITEITEN
-    sed -i "
-s#<a class='activity card '#<div class=\"activity-card card chapter\" #g
-s#</a>#</div>#g
-
-s#<h2>#<h4 class=\"card-title\">#g
-s#</h2>#</h4>#g
-
-s#<h3>#<div class=\"card-text\">#g
-s#</h3>#</div>#g
 " "$f"
 
 done
