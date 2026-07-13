9c74eccf6df9a2a3f4d9f09e2a1d6b173bf35316
Ingmar-1986 <herremaningmar@gmail.com>
2026-07-13 21:16:36 +0000

    Update testmodule

--- convertxourse.sh
+++ convertxourse.sh
@@ -5,11 +5,16 @@ do
     echo "Verwerk $f"
 
     # standalone.css vervangen door test.css
-    sed -i "s#<link href='https://ximera.osu.edu/public/stylesheets/standalone.css' media='screen' rel='stylesheet' />#<link href='https://ximera.osu.edu/public/stylesheets/standalone.css' media='screen' rel='stylesheet' /><link href='../test.css' media='screen' rel='stylesheet' />#g" "$f"
+    sed -i "s#<link href='https://ximera.osu.edu/public/stylesheets/standalone.css' media='screen' rel='stylesheet' />#<link href='https://ximera.osu.edu/public/stylesheets/standalone.css' media='screen' rel='stylesheet' /><link href='../test.css' media='screen' rel='stylesheet' /><link href='../Header_Footer.css' media='screen' rel='stylesheet' />#g" "$f"
 
     sed -i '/<body>/r ./header.html' "$f"
 
+sed -i '/<\/body>/i\
+PLACEHOLDER_FOOTER
+' "$f"
 
+sed -i '/PLACEHOLDER_FOOTER/r Footer.html' "$f"
+sed -i '/PLACEHOLDER_FOOTER/d' "$f"
     # Parts omzetten naar KU Leuven-achtige secties
     sed -i "
 s#<h1 class='card part' id='part1'>#<div class=\"activity-card card-sectionheading card part\"><div class=\"card-block\"><h4 class=\"card-title\">#g
